e5f2be8eb453918ba923768312652378c288bbea
fpellicano <fpellicano@newhaven.edu>
2022-09-22 18:18:23 -0400

    App Starting Point

--- app/src/main/java/com/example/demoapp1/MainActivity.kt
+++ app/src/main/java/com/example/demoapp1/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.demoapp1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Binder
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         // TODO: Write some okay code
         // TODO: Fix issues with previous xml file
-
-        thjdghj
-        fjfg
 
     }
 }
